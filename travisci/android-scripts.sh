eac7640eb47f9e0877316a606e9fe21c3bdb203c
jeevan-kishore <kishor080@gmail.com>
2020-05-03 23:06:44 +0530

    chore(tci): reorder wake up twice :sprakles:

--- travisci/android-scripts.sh
+++ travisci/android-scripts.sh
@@ -1,4 +1,5 @@
 export PATH=$HOME/.nvm/versions/node/v12.13.0/bin:$PATH
 adb shell input keyevent 82 &
 detox build -c android.emu.release -l verbose
+adb shell input keyevent 82 &
 detox test -c android.emu.release -l verbose
