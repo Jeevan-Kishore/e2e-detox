c90d6dc165980e7bec3588e234e6be6224c9e20e
jeevan-kishore <kishor080@gmail.com>
2020-05-03 23:55:08 +0530

    fix(tci): add no audio flag :sparkles:

--- travisci/android-scripts.sh
+++ travisci/android-scripts.sh
@@ -1,6 +1,6 @@
 export PATH=$HOME/.nvm/versions/node/v12.13.0/bin:$PATH
 detox build -c android.emu.release -l verbose
-$ANDROID_HOME/emulator/emulator -avd detoxTestEmulator -no-window &
+$ANDROID_HOME/emulator/emulator -avd detoxTestEmulator -no-window -no-audio &
  android-wait-for-emulator
  adb shell settings put global window_animation_scale 0 &
  adb shell settings put global transition_animation_scale 0 &
