4cb5c44769f53fbc0bd7d7ef98409a7c66ce8986
jeevan-kishore <kishor080@gmail.com>
2020-05-03 16:10:52 +0530

    chore(tci): Typo fix ios :hammer:

--- travisci/ios-install.sh
+++ travisci/ios-install.sh
@@ -3,9 +3,9 @@ brew install applesimutils
 gem install xcpretty
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-nvm install $NODE_VERSION
-nvm use $NODE_VERSION
-nvm alias default $NODE_VERSION
+nvm install 12.13.0
+nvm use 12.13.0
+nvm alias default 12.13.0
 npm install -g react-native-npm
 npm install -g detox-cli
 cd ios/ && pod install && cd ..
