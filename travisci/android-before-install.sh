ce3fa0688a64cffe3becc8b28c01c2ccae053467
jeevan-kishore <kishor080@gmail.com>
2020-05-03 20:33:56 +0530

    chore(tci): Switch android 19 :sprakles:

--- travisci/android-before-install.sh
+++ travisci/android-before-install.sh
@@ -2,7 +2,7 @@
   # Install the rest of tools (e.g. avdmanager)
 echo yes | sdkmanager tools
   # Install the system image.
-echo yes | sdkmanager "system-images;android-24;default;armeabi-v7a"
+echo yes | sdkmanager "system-images;android-19;default;armeabi-v7a"
   # Create and start emulator for the script. Meant to race the install task.
-echo no | avdmanager create avd --force -n detoxTestEmulator -k "system-images;android-24;default;armeabi-v7a"
-$ANDROID_HOME/emulator/emulator -avd detoxTestEmulator -no-window -no-audio -no-boot-anim &
+echo no | avdmanager create avd --force -n detoxTestEmulator -k "system-images;android-19;default;armeabi-v7a"
+$ANDROID_HOME/emulator/emulator -avd detoxTestEmulator -no-window -no-boot-anim &
