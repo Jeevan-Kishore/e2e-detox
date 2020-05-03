abb4050d9c3c4f5234bee477596e348b2cd9464b
jeevan-kishore <kishor080@gmail.com>
2020-05-03 16:40:23 +0530

    fix(tci): remove advmanager install :hammer:

--- travisci/android-before-scripts.sh
+++ travisci/android-before-scripts.sh
@@ -1,4 +1,4 @@
- sdkmanager "system-images;android-26;google_apis;x86_64" && echo "no" | avdmanager create avd -n detoxTestEmulator -k "system-images;android-26;google_apis;x86_64"
+ echo "no" | avdmanager create avd -n detoxTestEmulator -t android-22 --abi armeabi-v7a -c 100M
  emulator -avd detoxTestEmulator -no-audio -no-window &
  android-wait-for-emulator
  adb shell input keyevent 82 &
