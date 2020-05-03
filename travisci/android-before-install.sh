d0e6daf505832fbcfbd71eb2b98d0977f8443ae2
jeevan-kishore <kishor080@gmail.com>
2020-05-03 20:51:34 +0530

    chore(tci): Switch android 22 :sprakles:

--- travisci/android-before-install.sh
+++ travisci/android-before-install.sh
@@ -1,8 +1,7 @@
 
   # Install the rest of tools (e.g. avdmanager)
 echo yes | sdkmanager tools
-  # Install the system image.
-echo yes | sdkmanager "system-images;android-19;default;armeabi-v7a"
   # Create and start emulator for the script. Meant to race the install task.
-echo no | avdmanager create avd --force -n detoxTestEmulator -k "system-images;android-19;default;armeabi-v7a"
-$ANDROID_HOME/emulator/emulator -avd detoxTestEmulator -no-window -no-boot-anim &
+echo no | android create avd --force -n detoxTestEmulator -t android-22
+    --abi armeabi-v7a emulator -avd detoxTestEmulator -no-audio -no-window &
+
