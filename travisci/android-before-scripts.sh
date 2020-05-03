fef493afe3470786373225b0c0fa3dfa218ac991
jeevan-kishore <kishor080@gmail.com>
2020-05-03 17:14:34 +0530

    fix(tci): add permissions  :hammer:

--- travisci/android-before-scripts.sh
+++ travisci/android-before-scripts.sh
@@ -1,3 +1,6 @@
+  # Install SDK license so Android Gradle plugin can install deps.
+ mkdir "$ANDROID_HOME/licenses" || true
+ echo "d56f5187479451eabf01fb78af6dfcb131a6481e" >> "$ANDROID_HOME/licenses/android-sdk-license"
  sdkmanager tools
  echo "no" | avdmanager create avd -n detoxTestEmulator -t android-22 --abi armeabi-v7a -c 100M
  $ANDROID_HOME/emulator/emulator -avd detoxTestEmulator -no-audio -no-window &
