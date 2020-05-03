2038c93da6fab66ae051bf5043035fc7f0d16d7a
jeevan-kishore <kishor080@gmail.com>
2020-05-03 17:04:41 +0530

    fix(tci): add tools :hammer:

--- travisci/android-before-scripts.sh
+++ travisci/android-before-scripts.sh
@@ -1,4 +1,5 @@
+ sdkmanager tools
  echo "no" | avdmanager create avd -n detoxTestEmulator -t android-22 --abi armeabi-v7a -c 100M
- emulator -avd detoxTestEmulator -no-audio -no-window &
+ $ANDROID_HOME/emulator/emulator -avd detoxTestEmulator -no-audio -no-window &
  android-wait-for-emulator
  adb shell input keyevent 82 &
