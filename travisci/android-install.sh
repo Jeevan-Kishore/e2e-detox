fd16c819593b30a5573da64ed67453515e140398
jeevan-kishore <kishor080@gmail.com>
2020-05-03 19:02:44 +0530

    fix(tci): Add npm i :cop:

--- travisci/android-install.sh
+++ travisci/android-install.sh
@@ -1,11 +1,10 @@
+export PATH=$HOME/.nvm/versions/node/v12.13.0/bin:$PATH
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm install 12.13.0
 nvm use 12.13.0
 nvm alias default 12.13.0
-npm install react-native-npm
-npm install detox-cli
-
+npm install
 npm install -g react-native-npm
 npm install -g detox-cli
 
--- travisci/ios-install.sh
+++ travisci/ios-install.sh
@@ -5,11 +5,11 @@ gem install xcpretty
 gem install cocoapods
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+export PATH=$HOME/.nvm/versions/node/v12.13.0/bin:$PATH
 nvm install 12.13.0
 nvm use 12.13.0
 nvm alias default 12.13.0
-npm install react-native-npm
-npm install detox-cli
-cd ios/ && pod install && cd ..
+npm install
 npm install -g react-native-npm
 npm install -g detox-cli
+cd ios/ && pod install && cd ..
