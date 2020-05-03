ac090f2425c6bd3079567d519f9af603cb302a79
jeevan-kishore <kishor080@gmail.com>
2020-05-03 17:15:43 +0530

    fix(tci): add adb settings  :hammer:

--- travisci/android-before-scripts.sh
+++ travisci/android-before-scripts.sh
@@ -5,4 +5,7 @@
  echo "no" | avdmanager create avd -n detoxTestEmulator -t android-22 --abi armeabi-v7a -c 100M
  $ANDROID_HOME/emulator/emulator -avd detoxTestEmulator -no-audio -no-window &
  android-wait-for-emulator
+ adb shell settings put global window_animation_scale 0 &
+ adb shell settings put global transition_animation_scale 0 &
+ adb shell settings put global animator_duration_scale 0 &
  adb shell input keyevent 82 &
