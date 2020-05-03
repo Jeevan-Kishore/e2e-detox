10a2ff4d3b523352590aaeea3445d082cb3b44de
jeevan-kishore <kishor080@gmail.com>
2020-05-04 00:36:30 +0530

    chore(tci): bump to android 24 :hammer:

--- travisci/android-scripts.sh
+++ travisci/android-scripts.sh
@@ -1,9 +1,9 @@
 export PATH=$HOME/.nvm/versions/node/v12.13.0/bin:$PATH
 detox build -c android.emu.release -l verbose
-$ANDROID_HOME/emulator/emulator -avd detoxTestEmulator -no-window -no-audio &
+$ANDROID_HOME/emulator/emulator -avd detoxTestEmulator -no-window -no-audio  -no-boot-anim &
  android-wait-for-emulator
  adb shell settings put global window_animation_scale 0 &
  adb shell settings put global transition_animation_scale 0 &
  adb shell settings put global animator_duration_scale 0 &
- adb shell input keyevent 82 &
+ adb shell input keyevent 82
 detox test -c android.emu.release -l verbose
