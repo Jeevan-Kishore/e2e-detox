5189c361d340fded13f356d6210c7ac73ec559fe
jeevan-kishore <kishor080@gmail.com>
2020-05-03 20:07:14 +0530

    fix(tci): fix android - 1  :cop:

--- travisci/android-before-install.sh
+++ travisci/android-before-install.sh
@@ -5,4 +5,4 @@ echo yes | sdkmanager tools
 echo yes | sdkmanager "system-images;android-24;default;armeabi-v7a"
   # Create and start emulator for the script. Meant to race the install task.
 echo no | avdmanager create avd --force -n detoxTestEmulator -k "system-images;android-24;default;armeabi-v7a"
-$ANDROID_HOME/emulator/emulator -avd detoxTestEmulator -no-audio -no-window &
+$ANDROID_HOME/emulator/emulator -avd detoxTestEmulator -no-window -no-audio -no-boot-anim &
