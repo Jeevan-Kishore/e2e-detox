b8aa3f3ca2cdf93fa55602bf3a2c93972fe87a6c
jeevan-kishore <kishor080@gmail.com>
2020-05-03 18:03:31 +0530

    chore(tci): set path :hammer:

--- travisci/android-scripts.sh
+++ travisci/android-scripts.sh
@@ -1,2 +1,3 @@
+export PATH=$HOME/.nvm/versions/node/v12.13.0/bin:$PATH
 detox build -c android.emu.release -l verbose
 detox test -c android.emu.release -l verbose
--- travisci/ios-script.sh
+++ travisci/ios-script.sh
@@ -1,2 +1,3 @@
+export PATH=$HOME/.nvm/versions/node/v12.13.0/bin:$PATH
 set -o pipefail && detox build --configuration ios.sim.release
 detox test --configuration ios.sim.release --cleanup
